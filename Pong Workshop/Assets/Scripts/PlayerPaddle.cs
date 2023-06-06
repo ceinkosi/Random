164b639eb12a7798095b5aad050ecb650da8039a
ceinkosi <115333128+ceinkosi@users.noreply.github.com>
2023-06-06 10:23:45 +0200

    Updated Scripts

--- Pong Workshop/Assets/Scripts/PlayerPaddle.cs
+++ Pong Workshop/Assets/Scripts/PlayerPaddle.cs
@@ -2,8 +2,28 @@ using UnityEngine;
 
 public class PlayerPadel : Paddle
 {
+    private Vector2 _direction;
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else
+        {
+            _direction = Vector2.zero;
+        }
     }
+
+   private void FixedUpdate()
+   {
+        if(_direction.sqrMagnitude != 0)
+        {
+            rb.AddForce(_direction * this.speed);
+        }
+   }
 }
