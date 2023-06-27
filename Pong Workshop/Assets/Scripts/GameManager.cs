199b8ef5538ae8194a1e247e74531ae14077bf0f
ceinkosi <115333128+ceinkosi@users.noreply.github.com>
2023-06-27 09:46:29 +0200

    Wassup Again
    New Update

---
+++ Pong Workshop/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Ball ball;
+    public Paddle playerPaddle;
+    public int playerScore;
+    public TextMeshPro playerScoreText;
+
+    public Paddle computerPaddle;
+    public int computerScore;
+    public TextMeshPro computerScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.R))
+        {
+            NewGame();
+        }
+    }
+
+    public void NewGame()
+    {
+        SetPlayerScore(0);
+        SetComputerScore(0);
+        StartRound();
+    }
+
+    public void StartRound()
+    {
+        playerPaddle.ResetPosition();
+        computerPaddle.ResetPosition();
+        ball.ResetPosition();
+        ball.StartForce();
+    }
+
+    public void PlayerScores()
+    {
+        SetPlayerScore(playerScore + 1);
+        StartRound();
+    }
+
+     public void ComputerScores()
+    {
+        SetComputerScore(computerScore + 1);
+        StartRound();
+    }
+    
+    public void SetPlayerScore(int score)
+    {
+        playerScore = score;
+        playerScoreText.text = score.ToString();
+    }
+
+     public void SetComputerScore(int score)
+    {
+        computerScore = score;
+        computerScoreText.text = score.ToString();
+    }
+}
--- Pong Workshop/Assets/Scripts/Paddle.cs
+++ Pong Workshop/Assets/Scripts/Paddle.cs
@@ -10,4 +10,10 @@ public class Paddle : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
     }
+
+    public void ResetPosition()
+    {
+        rb.velocity = Vector2.zero;
+        rb.position = new Vector2(rb.position.x, 0f);
+    }
 }
